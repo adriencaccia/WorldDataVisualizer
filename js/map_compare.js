332f84a2842d41792db1ffe80ecf4d803337a5c4
Philippe Edwards <philippe.edwards@telecom-paristech.fr>
2018-06-25 23:40:55 +0200

    comparison card redone

--- js/map_compare.js
+++ js/map_compare.js
@@ -193,113 +193,106 @@ function render_map(){
 }
 
 function update_tooltip() {
+
 	if (country1 != null && country2!=null) {
 		features=data_country1=data_country2=[];
-		var card = document.getElementById("data_card");
-		card.style.visibility='visible';
-		var card_body = card.getElementsByClassName("card-body")[0];
-		card_body.getElementsByClassName("country1")[0].innerHTML="";
-		card_body.getElementsByClassName("country2")[0].innerHTML="";
-		card_body.getElementsByClassName("features")[0].innerHTML="";
-		
-		switch (data_type) {
-				case 'general':
-					features=['Area(m²)','Population(inh)']
-					data_country1=[country1_data.area_sqkm,country1_data.population]
-					data_country2=[country2_data.area_sqkm,country2_data.population]
+    var card = document.getElementById("data_card");
+    
+    card.style.visibility='visible';
+    var card_body = card.getElementsByClassName("card-body")[0];  
+		card.getElementsByClassName("country1")[0].innerHTML="";
+		card.getElementsByClassName("country2")[0].innerHTML="";
+    
+    var chart = d3.bullet().width(500).height(25);
+    card_body.innerHTML="";
+
+    d3.json("data/bullets.json", function(error, data) {
+      if (error) throw error;
+      
+      switch(data_type){
+        case 'general':
+          data = data.slice(0,2);
+					data_country1=[country1_data.area_sqkm,country1_data.population];
+          data_country2=[country2_data.area_sqkm,country2_data.population];
 					break;
-				case 'economics':
-					features = ["GDP(M.USD)","Inflation(%)","External Debt(M.USD)","Imports(M.USD)","Account Balance(M.USD)","Unemployment(%)","GDP per Capita(USD/inh)",
-					"GDP Growthrate(%)","Public Debt(% of GDP)","Exports(M.USD)","Investment(% of GDP)","Labor Force","Military Expenditures(M.USD)","Gold Reserves(kUSD)"]
+        case 'economics':
+          data = data.slice(2,16);
 					data_country1=[country1_data.gdp/1000000,country1_data.inflation,country1_data.debt_external/1000000,country1_data.imports/1000000,
 					country1_data.account_balance/1000000,country1_data.unemployment,country1_data.gdp_percapita,country1_data.gdp_growthrate,country1_data.debt_public,
-					country1_data.exports/1000000,country1_data.investment,country1_data.labor_force,country1_data.military_dollar/1000000,country1_data.gold_reserves/1000000]
+					country1_data.exports/1000000,country1_data.investment,country1_data.labor_force,country1_data.military_dollar/1000000,country1_data.gold_reserves/1000]
 
 					data_country2=[country2_data.gdp/1000000,country2_data.inflation,country2_data.debt_external/1000000,country2_data.imports/1000000,
-					country2_data.account_balance/1000000,country2_data.unemployment,country2_data.gdp_percapita,country2_data.gdp_growthrate,country2_data.debt_public/1000000,
-					country2_data.exports/1000000,country2_data.investment/1000000,country2_data.labor_force,country2_data.military_dollar/1000000,country2_data.gold_reserves/1000]
+					country2_data.account_balance/1000000,country2_data.unemployment,country2_data.gdp_percapita,country2_data.gdp_growthrate,country2_data.debt_public,
+					country2_data.exports/1000000,country2_data.investment,country2_data.labor_force,country2_data.military_dollar/1000000,country2_data.gold_reserves/1000]
 					break;
-				case 'resources':
-					features=["Electricity Production(GWh)","Gas Production(M.m&sup3)","Gaz Exports(M.m&sup3)","Electricity Consumption(GWh)","Gas Consumption(M.m&sup3)",
-					"Gaz Imports(1000 bbl/day)","Oil Consumption(1000 bbl/day)","Oil Imports(1000 bbl/day)"]
-					data_country1=[country1_data.elec_production/1000000,country1_data.gas_production/1000000,country1_data.gas_exports,
-					country1_data.elec_consumption/1000000,country1_data.gas_consumption/1000000,country1_data.gas_imports/1000,country1_data.oil_consumption/1000,
-					country1_data.oil_imports]
-					data_country2=[country2_data.elec_production,country2_data.gas_production,country2_data.gas_exports,
-					country2_data.elec_consumption,country2_data.gas_consumption,country2_data.gas_imports,country2_data.oil_consumption,
-					country2_data.oil_imports/1000]
+        case 'resources':
+          data = data.slice(16,26);
+          data_country1=[country1_data.elec_production/1000000,country1_data.elec_consumption/1000000,
+          country1_data.gas_production/1000000,country1_data.gas_imports/1000000,country1_data.gas_exports/1000000,country1_data.gas_consumption/1000000,          
+          country1_data.oil_production/1000,country1_data.oil_imports/1000,country1_data.oil_exports/1000,country1_data.oil_consumption/1000]
+          
+          data_country2=[country2_data.elec_production/1000000,country2_data.elec_consumption/1000000,
+          country2_data.gas_production/1000000,country2_data.gas_imports/1000000,country2_data.gas_exports/1000000,country2_data.gas_consumption/1000000,          
+          country2_data.oil_production/1000,country2_data.oil_imports/1000,country2_data.oil_exports/1000,country2_data.oil_consumption/1000]
 					break;
-				case 'health':
-					features=["Birth Rate(/1000 people)","Life Expectancy (%)","People living with HIV","HIV prevalence rate(%)","Fertility rate(%)"]
+        case 'health':
+          data = data.slice(26,31);
 					data_country1=[country1_data.birth_rate,country1_data.life_expectancy,country1_data.hiv_living,country1_data.hiv_prevalence,country1_data.fertility_rate]
 					data_country2=[country2_data.birth_rate,country2_data.life_expectancy,country2_data.hiv_living,country2_data.hiv_prevalence,country2_data.fertility_rate]
 					break;
-				case 'tech':
-					features=["Highways(km)","Internet Hosts","Telephone Mainlines","Railways","Internet Users","Mobile Lines"]
+        case 'tech':
+          data = data.slice(31,37);
 					data_country1=[country1_data.highways,country1_data.internet_hosts,country1_data.telephone_mainlines,country1_data.railways,
 					country1_data.internet_users,country1_data.telephone_mobile]
 					data_country2=[country2_data.highways,country2_data.internet_hosts,country2_data.telephone_mainlines,country2_data.railways,
 					country2_data.internet_users,country2_data.telephone_mobile]
 					break;
-			}
+      }
+      for(var i=0; i<data.length; i++){
+        data[i].markers = [0];
+        data[i].ranges = [data_country1[i]];
+        data[i].measures = [data_country2[i]];
+      }
+      console.log(data);
+      
+      var bulletSvg = d3.select("#cardBody").selectAll("svg")
+          .data(data)
+        .enter().append("svg")
+          .attr("class", "bullet")
+          .attr("width", 900)
+          .attr("height", 60)
+        .append("g")
+          .attr("transform", "translate(" + 200 + "," + 8 + ")")
+          .call(chart);
+    
+      var title = bulletSvg.append("g")
+          .style("text-anchor", "end")
+          .attr("transform", "translate(-8, 15)");
+    
+      title.append("text")
+          .attr("class", "title")
+          .text(function(d) { return d.title; });
+    
+      title.append("text")
+          .attr("class", "subtitle")
+          .attr("dy", "1em")
+          .text(function(d) { return d.subtitle; });
+
+    });
 
 
+    var c1= document.createElement("div")
+      c1.innerHTML = "<h3>" + country1_data.country.toLocaleString("en-US")+"</h3>";
+      c1.setAttribute("class","row");
+    var c2= document.createElement("div")
+      c2.innerHTML = "<h3>" + country2_data.country.toLocaleString("en-US")+"</h3>";
+      c2.setAttribute("class","row");
+      
+    var c1_html = card.getElementsByClassName("card-countries")[0].getElementsByClassName("country1")[0];
+    var c2_html = card.getElementsByClassName("card-countries")[0].getElementsByClassName("country2")[0];
 
-
-				var c1= document.createElement("div")
-					c1.innerHTML = "<h3>" + country1_data.country.toLocaleString("en-US")+"</h3>";
-					c1.setAttribute("class","row");
-				var c2= document.createElement("div")
-					c2.innerHTML = "<h3>"+country2_data.country.toLocaleString("en-US")+"</h3>";
-					c2.setAttribute("class","row");
-				var f = document.createElement("div")
-					f.innerHTML = "<h3><br></h3>";
-					f.setAttribute("class","row");
-					
-					var c1_html = card_body.getElementsByClassName("country1")[0];
-					var c2_html = card_body.getElementsByClassName("country2")[0];
-					var f_html= card_body.getElementsByClassName("features")[0];
-
-
-
-					c1_html.appendChild(c1);
-					c2_html.appendChild(c2);
-					f_html.appendChild(f);
-
-
-			for(var i=0 ; i<features.length;i++){
-
-
-				c1= document.createElement("div")
-          c1.innerHTML = data_country1[i].toLocaleString("en-US");
-					c1.setAttribute("class","row");
-				c2= document.createElement("div")
-					c2.innerHTML = data_country2[i].toLocaleString("en-US");
-					c2.setAttribute("class","row");
-				f = document.createElement("div")
-					f.innerHTML = "<b>"+features[i].toLocaleString("en-US")+"<b>";
-					f.setAttribute("class","row");
-					
-        if (data_country1[i] < data_country2[i]) {
-          c1.style='color:#e8201e';
-          c2.style='color:#36ad29';
-        }
-        else if (data_country1[i] > data_country2[i]) {
-          c1.style='color:#36ad29';
-          c2.style='color:#e8201e';
-        }
-        else {
-          c1.style='color:#4a5df1';
-          c2.style='color:#4a5df1';
-        }
-
-					c1_html.appendChild(c1);
-					c2_html.appendChild(c2);
-					f_html.appendChild(f);
-					}
-		
-	
-
+    c1_html.appendChild(c1);
+    c2_html.appendChild(c2);
 	}
 }
 
@@ -309,12 +302,14 @@ function change(str){
 	for(i = 0;i < lis.length; i++){
 		var a=lis[i].getElementsByClassName('nav-link')[0];
 		if(lis[i].id==str && !(a.classList.contains('active'))){
-				a.classList.add('active');
+			a.classList.add('active');
 		}
 		else if(lis[i].id!=str){
 			a.classList.remove('active')
 		}
 	}
-	data_type = str;
+  data_type = str;
+  card_body = document.getElementById('data_card').getElementsByClassName('card-body')[0];
+  console.log(card_body);
 	update_tooltip();
 }
